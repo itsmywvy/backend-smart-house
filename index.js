037dc7e5d9840d74664b9285635628c003e2db13
David Kondzharia <dkondzharia@gmail.com>
2023-04-10 23:13:03 +0300

    Fixed problem with mongoDB

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const members = require('./routes/members');
 const rooms = require('./routes/rooms');
 const users = require('./routes/users');
 const billingChart = require('./routes/billingChart');
 
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+
 const app = express();
 
+app.use(compression());
+app.use(helmet());
 app.use(cors());
+app.use(limiter);
 app.use(express.json({ limit: '50mb' })); // allows us to accept the data in JSON format.
 app.use('/api', members);
 app.use('/api', rooms);
@@ -22,7 +33,10 @@ app.listen(3001, () => {
 
 require('dotenv').config();
 
-const mongoString = process.env.DATABASE_URL;
+const dev_db_url =
+  'mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority';
+
+const mongoString = process.env.DATABASE_URL || dev_db_url;
 
 mongoose.connect(mongoString);
 const database = mongoose.connection;
